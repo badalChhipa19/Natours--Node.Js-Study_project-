5e3916800d2f4522f1b7d6cb5f023eed183a5205
Badal <badal@NETURON17>
2023-09-12 18:43:49 +0530

    Handel errors from eslint

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
+const dotenv = require(`dotenv`);
 
-const app = require('./app');
+dotenv.config({ path: `./config.env` });
+const app = require(`./app`);
 
 //TODO N:  Running the server on port 3000
 const port = process.env.port || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on ${port} prot...`);
-});
+app.listen(port);
